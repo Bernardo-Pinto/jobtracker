39ff266c963876a8c1fb5665a297b052e465fb2b
Bernardo Pinto <bernardo.ra.pinto@gmail.com>
2025-08-15 11:53:22 +0200

    Refactor Applications component to include data fetching logic and handle loading state

--- app/(pages)/applications/page.tsx
+++ app/(pages)/applications/page.tsx
@@ -1,7 +1,54 @@
+'use client';
+import { useState, useEffect } from "react";
 import CustomDataGrid from "../../../components/data-grid"
 
-export default function applications() {
-    'use client';
-    return (
-        <CustomDataGrid />
+async function fetchApplicationData(){
+
+    try {
+        const response = await fetch('/api/application', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+    
+        if (!response.ok) {
+            console.log(' error: ' + response.statusText)
+            throw new Error('Failed to get applications');
+        }
+    
+        const result = await response.json();
+        console.log(result.message); // "Application added successfully"
+        return result;
+    } catch (error) {
+        console.error('Error gettins applications:', error);
+    }
+}
+
+export default function Applications() {
+    const [applicationList, setApplicationList] = useState([])
+    const [loading, setLoading] = useState(true);
+    const [updatedApplication, setUpdatedApplication] = useState(false)
+
+      // Fetch data on component mount
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const data = await fetchApplicationData();
+                setApplicationList(data);
+              } catch (error) {
+                console.log('error fetching data: ' + error);
+                //setError(error.message); //not implemented
+              } finally {
+                setLoading(false);
+            }
+        };
+        fetchData(); //run on mount
+    }, [updatedApplication]);
+
+    if (loading) {
+        return <div>Loading...</div>;
+      }
+     else return (
+        <CustomDataGrid applicationsData={applicationList} funcUpdatedApplication={setUpdatedApplication}/>
 )}
